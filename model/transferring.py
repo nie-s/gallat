cec2a08659932cf820ce25f733b396685dd6a6df
张瑞星 <19373115@buaa.edu.cn>
2021-12-08 01:11:49 +0800

    fix_little_bug_1_11_12_8

--- model/transferring.py
+++ model/transferring.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from model.attention_net import attention_net
+import torch.nn.functional as F
 
 
 class transferring_attention(nn.Module):
@@ -22,10 +23,11 @@ class transferring_attention(nn.Module):
     def forward(self, mt):
         q = self.attention_net.forward(mt, mt, torch.ones(self.m_size, self.m_size))  # n x n
         # print(q)
-        d = self.act(self.wd(mt))
-        # print(q)
-        # print(d)
-        q = torch.mul(q, d)
+        temp = self.wd(mt)
+        d = F.relu(temp) # todo 这里好奇怪，需要单独分出一个变量 不然d全是0
+
+        q = torch.mul(q, d) # 这里测试了一下，应该结果不会有问题
+
         '''
         for i in range(self.m_size):
             q[i] = torch.mul(q[i],
